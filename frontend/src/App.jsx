060a40dab827859a708f032e952d03f853f0d524
gideonsivak <gideonsivak2007@gmail.com>
2026-09-24 13:49:12 +0530

    Added supplier frontend routes in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,11 +3,19 @@ import {
     Routes,
     Route
 } from "react-router-dom";
+
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Navbar from "./components/Navbar";
 
 import BuyerDashboard from "./pages/buyer/BuyerDashboard";
+
+import SupplierDashboard from "./pages/supplier/SupplierDashboard";
+import SupplierRFQDetails from "./pages/supplier/SupplierRFQDetails";
+import QuotationForm from "./pages/supplier/QuotationForm";
+import MyQuotations from "./pages/supplier/MyQuotations";
+import SupplierAwardResult from "./pages/supplier/AwardResult";
+
 
 function Home() {
     return (
@@ -15,6 +23,7 @@ function Home() {
         </main>
     );
 }
+
 
 function App() {
     return (
@@ -27,35 +36,96 @@ function App() {
                     element={<Login />}
                 />
 
-                 <Route
+                <Route
                     path="/register"
                     element={<Register />}
                 />
+
+
+                {/* =========================
+                    BUYER
+                ========================= */}
+
+                <Route
+                    path="/buyer"
+                    element={
+                        <Navbar>
+                            <BuyerDashboard />
+                        </Navbar>
+                    }
+                />
+
+
+                {/* =========================
+                    SUPPLIER
+                ========================= */}
+
+                <Route
+                    path="/supplier"
+                    element={
+                        <Navbar>
+                            <SupplierDashboard />
+                        </Navbar>
+                    }
+                />
+
+                <Route
+                    path="/supplier/rfqs/:rfqId"
+                    element={
+                        <Navbar>
+                            <SupplierRFQDetails />
+                        </Navbar>
+                    }
+                />
+
+                <Route
+                    path="/supplier/rfqs/:rfqId/quotation"
+                    element={
+                        <Navbar>
+                            <QuotationForm />
+                        </Navbar>
+                    }
+                />
+
+                <Route
+                    path="/supplier/quotations"
+                    element={
+                        <Navbar>
+                            <MyQuotations />
+                        </Navbar>
+                    }
+                />
+
+                <Route
+                    path="/supplier/rfqs/:rfqId/award"
+                    element={
+                        <Navbar>
+                            <SupplierAwardResult />
+                        </Navbar>
+                    }
+                />
+
+
+                {/* =========================
+                    FALLBACK
+                ========================= */}
 
                 <Route
                     path="*"
                     element={
                         <>
-                            <Navbar />
-                            <Home />
+                            <Navbar>
+                                <Home />
+                            </Navbar>
                         </>
                     }
                 />
-                 <Route
-    path="/buyer"
-    element={
-        <Navbar>
-            <BuyerDashboard />
-        </Navbar>
-    }
-/>
+
             </Routes>
-
-
-          
 
         </BrowserRouter>
     );
 }
+
 
 export default App;
